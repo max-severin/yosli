a47ff76ed5a9be4287777b7a79d8aac277fb23a7
Max Severin <makc.severin@gmail.com>
2015-05-07 04:39:38 +0300

    shopYosliPlugin code review

--- lib/shopYosli.plugin.php
+++ lib/shopYosli.plugin.php
@@ -2,36 +2,39 @@
 
 /*
  * Class shopYosliPlugin
+ * Plugin allows users in the backend to add pictures with title and link,
+ * pictures are displayed at the frontend,
+ * jquery plagin that is used in the yosli plugin is a nivo slider - https://github.com/gilbitron/Nivo-Slider
  * @author Max Severin <makc.severin@gmail.com>
  */
 class shopYosliPlugin extends shopPlugin {
-
-    /** Handler for backend_menu event: return plugin button in backend menu. */
+    
+    /**
+     * Handler for backend_menu event: add yosli button in backend menu tabs
+     * @return array
+     */
     public function backendMenu() {
+        $html = '';
         
-        if ( $this->getSettings('status') === 'on' ) {
-
+        if ($this->getSettings('status') === 'on') {
 	        $html = '<li ' . (waRequest::get('plugin') == $this->id ? 'class="selected"' : 'class="no-tab"') . '>
 	                    <a href="?plugin=yosli">Слайдер</a>
 	                </li>';
-
-	        return array('core_li' => $html);
-
-        } else {
-
-        	return;
-
         }
+
+        return array('core_li' => $html);
     }
 
-    /** Method for frontend which displays jquery slider. */
+    
+    /**
+     * Frontend method that displays and initiates jquery nivo slider
+     * @return string
+     */
     static function display() {
-
         $app_settings_model = new waAppSettingsModel();
-
         $settings = $app_settings_model->get(array('shop', 'yosli'));
 
-        if ($settings['status']) {
+        if ($settings['status'] === 'on') {
 
             $model = new shopYosliPluginSlidesModel();
             $slides = $model->order('id DESC')->fetchAll();
@@ -41,15 +44,14 @@ class shopYosliPlugin extends shopPlugin {
             $view->assign('slides', $slides);
 
             $html = $view->fetch(realpath(dirname(__FILE__)."/../").'/templates/Frontend.html');
-
+            
             return $html;
 
         } else {
 
             return;
 
-        }        
-
+        }     
     }
 
 }
